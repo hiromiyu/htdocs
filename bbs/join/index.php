0a6dbe1fcce315e8ba8a67bb0abcbffc79d80581
ひろ <hiroyuki09171016@gmail.com>
2023-02-13 21:08:49 +0900

    書き直すの実装

--- bbs/join/index.php
+++ bbs/join/index.php
@@ -4,11 +4,15 @@ require('../library.php');
 
 $finfo = new finfo();
 
-$form = [
-    'name' => '',
-    'email' => '',
-    'password' => '',
-];
+if (isset($_GET['action']) && $_GET['action'] === 'rewrite' && isset($_SESSION['form'])) {
+    $form = $_SESSION['form'];
+} else {
+    $form = [
+        'name' => '',
+        'email' => '',
+        'password' => '',
+    ];
+}
 $error = [];
 
 
